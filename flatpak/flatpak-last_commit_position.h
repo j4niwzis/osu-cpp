a0e1aec7ee6e203604a02c868e4733453ace1cb9
j4niwzis <j4niwzis@proton.me>
2026-08-26 05:13:45 +0000

    Add Flatpak packaging

---
+++ flatpak/flatpak-last_commit_position.h
@@ -0,0 +1,9 @@
+// Generated version data is unavailable in flatpak-builder's tagless source
+// checkout. GN uses these values only for `gn --version`.
+#ifndef OUT_LAST_COMMIT_POSITION_H_
+#define OUT_LAST_COMMIT_POSITION_H_
+
+#define LAST_COMMIT_POSITION_NUM 0
+#define LAST_COMMIT_POSITION "0 (e0a6ab04a113)"
+
+#endif  // OUT_LAST_COMMIT_POSITION_H_
